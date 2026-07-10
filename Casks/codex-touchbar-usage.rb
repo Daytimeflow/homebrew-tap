1183c714fab4ffd9cdd2ebda953f32b736af3ec2
Daytimeflow <ziqiaowang10@gmail.com>
2026-07-11 07:53:53 +0800

    Update Codex Touch Bar Usage to 0.3.4

--- Casks/codex-touchbar-usage.rb
+++ Casks/codex-touchbar-usage.rb
@@ -1,6 +1,6 @@
 cask "codex-touchbar-usage" do
-  version "0.3.3"
-  sha256 "0e85db2c0a0c6b88f22c397612faf4bf5b1feb5dc92f876c63304ba51055336a"
+  version "0.3.4"
+  sha256 "0cfba47713828c508d069c050468e573e746d72cbb51cce2be658b234be79f35"
 
   url "https://github.com/Daytimeflow/codex-touchbar-usage/archive/refs/tags/v#{version}.tar.gz"
   name "Codex Touch Bar Usage"
